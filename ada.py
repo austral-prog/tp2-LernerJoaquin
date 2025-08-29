c83bb11a1ed46f3b24117518a982dadf4e4b1f9c
LernerJoaquin <lernerjoaquin@gmail.com>
2025-08-29 11:55:37 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,7 +1,7 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    print(first_name.lower(), last_name.lower())
-    print(first_name.title(), last_name.title())
-    print(first_name.upper(), last_name.upper())
-    print("\t", first_name.lower(), last_name.lower())
+    print(f"{first_name.lower()} {last_name.lower()}")
+    print(f"{first_name.title()} {last_name.title()}")
+    print(f"{first_name.upper()} {last_name.upper()}")
+    print(f"\t{first_name.lower()} {last_name.lower()}")
